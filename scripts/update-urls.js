59e6ef0d7f3b038e98d5c22290b0b6c074168019
mov01 <314829976@qq.com>
2024-07-19 02:28:52 +0800

    Update update-urls.js

--- scripts/update-urls.js
+++ scripts/update-urls.js
@@ -2,15 +2,14 @@ import fetch from 'node-fetch';
 const path = require('path');
 const fs = require('fs');
 
-const githubUsername = 'vipm01'; // GitHub 用户名
-const repoName = 'vipm01.github.io'; // GitHub Pages 所在的仓库名称
-const filePath = 'urls.json'; // 存储 URL 映射的文件路径，位于仓库根目录
+const githubUsername = 'vipm01';
+const repoName = 'vipm01.github.io';
+const filePath = 'urls.json';
 
-const githubToken = process.env.GH_TOKEN; // 从环境变量中获取 GitHub 个人访问令牌
+const githubToken = process.env.GH_TOKEN;
 
 async function saveURLs(urls) {
     try {
-        // 获取现有文件的 SHA 值
         const response = await fetch(`https://api.github.com/repos/${githubUsername}/${repoName}/contents/${filePath}`, {
             headers: {
                 'Authorization': `token ${githubToken}`,
@@ -41,12 +40,10 @@ async function saveURLs(urls) {
     }
 }
 
-// 读取当前的 urls.json
 const urls = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-// 生成一个新的短链接并保存
 const shortCode = Math.random().toString(36).substring(2, 8);
-urls[shortCode] = process.argv[2]; // 从命令行参数中获取要缩短的 URL
+urls[shortCode] = process.argv[2];
 
 saveURLs(urls).then(success => {
     if (success) {
