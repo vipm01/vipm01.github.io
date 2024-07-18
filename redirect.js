026caf699377413ae2dc666c15b207a613758e84
mov01 <314829976@qq.com>
2024-07-18 20:07:04 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,9 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => response.text())
     .then(url => {
         if (url) {
-            setTimeout(function() {
-                window.location.href = url;
-            }, 1000); // 延迟3秒后跳转
+            // 检查是否在 QQ 浏览器中访问
+            if (navigator.userAgent.match(/QQBrowser/i)) {
+                // 使用外部浏览器打开 URL
+                window.open(url, '_blank');
+            } else {
+                // 延迟3秒后在当前窗口跳转
+                setTimeout(function() {
+                    window.location.href = url;
+                }, 1000);
+            }
         } else {
             console.error('No URL found in redirect-url.txt');
         }
