bee9473fc743a0afc43f103cb2694450dbb6e0ef
mov01 <314829976@qq.com>
2024-07-19 00:13:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ function generateShortUrl() {
     const shortUrl = document.getElementById('shortUrl');
 
     if (originalUrl) {
-        const fakeShortUrl = `https://vipm01.github.io/#${encodeURIComponent(originalUrl)}`;
-        shortUrl.textContent = "生成的短链接: " + fakeShortUrl;
+        fetch('/generate-short-url', {
+            method: 'POST',
+            body: JSON.stringify({ originalUrl }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.text())
+        .then(data => {
+            shortUrl.textContent = "生成的短链接: " + data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            shortUrl.textContent = "生成短链接失败";
+        });
     } else {
         shortUrl.textContent = "请输入原始URL";
     }
