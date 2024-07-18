eab74dc9cf55dda17f17baf3f84742dab34b7584
mov01 <314829976@qq.com>
2024-07-18 19:44:54 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,7 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('/dist/redirect-url.txt')
     .then(response => response.text())
     .then(url => {
-        window.location.href = url;
+        if (url) {
+            setTimeout(function() {
+                window.location.href = url;
+            }, 1000); // 延迟3秒后跳转
+        } else {
+            console.error('No URL found in redirect-url.txt');
+        }
     })
     .catch(error => {
         console.error('Error fetching redirect URL:', error);
